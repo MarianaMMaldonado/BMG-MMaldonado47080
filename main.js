b2983f75a3cd3807b2c2d45ad322b235cdc2de13
marianamm <marianela.mtz.mal@gmail.com>
2023-10-13 19:56:41 -0600

    generar js en segunda página de html

--- main.js
+++ main.js
@@ -1,14 +1,55 @@
-let entrada = prompt("¿Cuál es tu color favorito? (en minúsculas):");
+let carrito = []
+let nombre = prompt("Ingresa tu nombre: ");
+let servicios = ["web", "pdf", "basico", "plus"]
+let costoServicios = [{web: 1500}, {pdf: 900}, {basico: 3000}, {plus: 7000}]
 
-while (entrada !="salir"){
-    if (entrada === "amarillo"){
-        alert("Amarillo es energía, alegría, felicidad, riqueza, poder, fuerza y acción")
-    } else if (entrada === "azul"){
-        alert("Azul es serenidad, paz, protección, calma y confianza")
-    } else if (entrada === "morado"){
-        alert ("Morado es sabiduría, poder, creatividad y magia")
-    } else {
-        alert("¡Ups! No tengo ese color aún")
-    };
-    entrada = prompt ("Escribe otro color o para cerrar la venta escribe 'salir'.")
+const persona1 = {
+    nombre: nombre,
+    total: 0
 }
+
+let servicio = prompt("¿Qué paquete te gustaría agregar? " + nombreServicios(servicios));
+
+while (servicio != "ESC"){
+    if(servicios.includes(servicio)){
+        let costo = buscarCostoServicio(servicio)
+        carrito.push(costo)
+    }else{
+        alert("Dato no válido")
+    };
+
+    servicio = prompt("Escribe ESC para salir y obtener el total de tu compra o selecciona otro servicio " + nombreServicios(servicios))
+}
+
+persona1["total"] = sumarTotal(carrito)
+
+alert(persona1["nombre"] + " el total de tu compra es: " + persona1["total"])
+
+
+function sumarTotal(carrito) {
+    let sum = 0;
+
+    for(const num of carrito){
+        sum += num;
+    }
+    return sum;
+  }
+
+  function nombreServicios(servicios){
+    let listaServicios = "";
+
+    for(const servicio of servicios){
+        listaServicios += servicio + " ";
+    }
+    return listaServicios;
+  }
+
+  function buscarCostoServicio(servicio){
+    for(const costos of costoServicios){
+        if(Object.keys(costos)[0] == servicio){
+            return costos[servicio]
+        }
+    }
+  }
+
+
